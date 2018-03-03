4416ba8280c513ba3ff66d9a18f565adec7a3e32
Ryan Trad <trad.ryan.alan@gmail.com>
2018-03-03 16:59:37 -0500

    updated server with new coords

--- server.py
+++ server.py
@@ -9,16 +9,28 @@ db = dbclient.squiggledoobs
 app = Flask(__name__)
 
 agg = {
-        0 : {
-            'location' : (33.781799, -84.368374),
-            'activity_v' : 0
-            },
         1 : {
-            'location' : (33.774839, -84.364906),
+            'location' : (33.7816899, -84.3683153),
             'activity_v' : 0
             },
         2 : {
-            'location' : (33.768096, -84.361430),
+            'location' : (33.7806076, -84.3673886),
+            'activity_v' : 0
+            },
+        3 : {
+            'location' : (33.7800137, -84.3668665),
+            'activity_v' : 0
+            },
+        4 : {
+            'location' : (33.7794260, -84.3664867),
+            'activity_v' : 0
+            },
+        5 : {
+            'location' : (33.7780078, -84.3657849),
+            'activity_v' : 0
+            },
+        6 : {
+            'location' : (33.7744780, -84.3647942),
             'activity_v' : 0
             }
     }
@@ -26,8 +38,11 @@ agg = {
 
 @app.route('/')
 def index():
-    time_upper = (datetime.datetime.now() - datetime.datetime(1970,1,1))
-    time_lower = (time_upper - datetime.timedelta(minutes = 10)).total_seconds()
+    # time_upper = (datetime.datetime.now() - datetime.datetime(1970,1,1))
+    # time_lower = (time_upper - datetime.timedelta(minutes = 10)).total_seconds()
+    
+    time_lower = 1520084629.944
+
     # recordings = db.recordings.find()
     recordings = db.recordings.find({'timestamp' : {'$gte' : time_lower}})
     for rec in recordings:
