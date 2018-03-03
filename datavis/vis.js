2031f3f3359ed477ee7d7e3a67b3d0ab9e6b6835
rgolding <golding.rh@gmail.com>
2018-03-03 12:58:29 -0500

    created heatmap

--- datavis/vis.js
+++ datavis/vis.js
@@ -1,22 +1,67 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: {lat: -33.865427, lng: 151.196123},
+    zoom: 11,
+    center: {lng:-84.3880 , lat: 33.7490},
     mapTypeId: 'terrain'
   });
 
   // Create a <script> tag and set the USGS URL as the source.
-  var script = document.createElement('script');
+  // var script = document.createElement('script');
 
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
+  // JSON OBJECT FROM HARDWARE
+  // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+  // document.getElementsByTagName('head')[0].appendChild(script);
+
+  var results = {{ data | tojson}};
+
+  var heatmapData = [];
+
+  for (var i = 0; i < results.length; i++) {
+
+    var latLng = new google.maps.LatLng(results[i].location[0], results[i].location[1])''
+    var magnitude = results[i].activity_v;
+
+    var weightedLoc = {
+      location : latLng,
+      weight : magnitude
+    };
+    heatmapData.push(weightedLoc);
+  }
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data:heatmapData,
+    dissipating:false,
+    map: map
+  });
+
 
 }
 
 function eqfeed_callback(results) {
+  var heatmapData = [];
+  for (var i = 0; i &lt; results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    var magnitude = results.features[i].properties.mag;
+    var weightedLoc = {
+      location: latLng,
+      weight: Math.pow(2, magnitude)
+    };
+    heatmapData.push(weightedLoc);
+  }
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map
+  });
+}
+
+
+
+
+
+function eqfeed_callback(results) {
+  console.log('callback');
   var heatmapData = [];
   for (var i = 0; i < results.features.length; i++) {
     var coords = results.features[i].geometry.coordinates;
