0f3294e69d30035f8c790f04b5074a8101a370d3
Ryan Trad <trad.ryan.alan@gmail.com>
2018-03-02 23:22:52 -0500

    fixed data format from server

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, redirect, render_template, jsonify, g
+from flask import Flask, request, redirect, render_template, json, g
 from pymongo import MongoClient
 
 dbclient = MongoClient('localhost', 27017)
+db = dbclient.squiggledoobs
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data={"location" : (80,80), "people" : 40, "timestamp" : 1520046817})
+    return render_template('index.html', data=[{ 'device_id' : 0, 'location' : (80,80), 'people' : 40, 'timestamp' : 1520046817}])
     
-#@app.route('/api/data/')
+# @app.route('/api/data/', methods = ['POST'])
+# def add_data():
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
