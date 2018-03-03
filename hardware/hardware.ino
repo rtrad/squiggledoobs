5d3b39e52ba360be45a6afd405630b54964f7873
Ryan Trad <trad.ryan.alan@gmail.com>
2018-03-02 21:53:29 -0500

    modified hardware.ino for updated lib

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,43 +1,38 @@
-/*
 
-*/
 
-#include <MeOrion.h>
+#include "MeOrion.h"
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <stdint.h>
 
 
-
-
 MeRGBLed led(PORT_3);
 
-int ledCount = 15;
-float j,f,k;
+MePIRMotionSensor PIRmotion_val(PORT_6);
 
+bool detected = 0;
 
 void setup() {
-  led.setNumber(ledCount);
+  Serial.begin(9600);
 }
 
 void loop() {
-  color_loop();
-  
+  led.setColorAt(0,0,0,0);
+  led.show();
+  detected = PIRmotion_val.isHumanDetected();
+  if(detected) {
+    color();
+  }
+
+  Serial.print(detected);
+
+  delay(100);
 }
 
-void color_loop() {
-  
-  for (uint8_t t = 0; t<ledCount; t++) {
-    uint8_t red = 64 * (1 + sin(t/2.0 + j/4.0));
-    uint8_t green = 64 * (1 + sin(t/2.0 + f/9.0 +2.1));
-    uint8_t blue = 64 * (1 + sin(t/2.0 + k/14.0 +4.2));
-    
-    led.setColorAt(t, red, green, blue);
-  }
+void color() {
+  uint8_t r = 250;
+
+  led.setColorAt(0, r, 0,0);
   led.show();
-  j+= random(1,6)/6.0;
-  k+= random(1,6)/6.0;
-  f+= random(1,6)/6.0;
-  
-  
 }
+
