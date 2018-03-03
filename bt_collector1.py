3953cf900c6c99d24caef279dc396f04af25b00f
Ryan Trad <trad.ryan.alan@gmail.com>
2018-03-03 13:57:29 -0500

    up to date viz with working db

--- bt_collector1.py
+++ bt_collector1.py
@@ -8,12 +8,6 @@ db = dbclient.squiggledoobs
 recordings = db.recordings
 
 
-device_location_map = {
-        0 : (33.781799, -84.368374),
-        1 : (33.780853, -84.367591),
-        2 : (33.780228, -84.367065)
-    }
-
 
 def main():
     ser = serial.Serial('COM8', 115200, timeout=5)
@@ -26,7 +20,7 @@ def main():
                 recording = {
                         'timestamp' : (datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds(),
                         'activity_v' : int(activity_v),
-                        'location' : device_location_map[0]
+                        'device_id' : 0
                     }
                 print recordings.insert_one(recording)
             except:
--- server.py
+++ server.py
@@ -1,20 +1,43 @@
 from flask import Flask, request, redirect, render_template, json, g
 from pymongo import MongoClient
 from bson.json_util import dumps
+from bson.code import Code
 import datetime
 
 dbclient = MongoClient('localhost', 27017)
 db = dbclient.squiggledoobs
 app = Flask(__name__)
 
+agg = {
+        0 : {
+            'location' : (33.781799, -84.368374),
+            'activity_v' : 0
+            },
+        1 : {
+            'location' : (33.774839, -84.364906),
+            'activity_v' : 0
+            },
+        2 : {
+            'location' : (33.768096, -84.361430),
+            'activity_v' : 0
+            }
+    }
+
 
 @app.route('/')
 def index():
-    time_upper = (datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds()
-    time_lower = time_upper - 30
+    time_upper = (datetime.datetime.now() - datetime.datetime(1970,1,1))
+    time_lower = (time_upper - datetime.timedelta(minutes = 10)).total_seconds()
+    # recordings = db.recordings.find()
     recordings = db.recordings.find({'timestamp' : {'$gte' : time_lower}})
-    data = dumps(recordings)
-    return render_template('index.html', data=data)
+    for rec in recordings:
+        agg[rec['device_id']]['activity_v'] += rec['activity_v']
+    # print agg
+    data = dumps(agg)
+    # data[1]
+    
+    print data[1]
+    return render_template('index.html', data=agg)
         
 
 
